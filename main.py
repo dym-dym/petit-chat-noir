1a95a613e61e07fed0f7441d2ee5b388f108c0ff
Dylan Bettendroffer <dylan.bettendroffer@gmail.com>
2023-10-05 15:06:52 +0200

    WIP on graph implementation

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from bs4 import BeautifulSoup
+import json
 import request3
+import rustworkx as rx
+import matplotlib.pyplot as plt
+from rustworkx.visualization import mpl_draw
+from bs4 import BeautifulSoup
+from jdm_types import parse_type, parse_relation_type, parse_relation, parse_node, Node, Node_Type, Relation_Type, R_Relation
 
 
 # Fetches relationnal data from jeuxdemots for
@@ -22,70 +27,78 @@ def fetch_word_data(word: str) -> str | None:
 
         return None
 
-class R_Relation:
-    def __init__(self, rid: int, out_node: int, in_node: int, r_type: int, weight: int):
-        self.rid = rid
-        self.out_node = out_node 
-        self.in_node = in_node
-        self.r_type = r_type
-        self.weight = weight
 
-    def __str__(self):
-        return f"Relation id : {self.rid}\nRelation origin: {self.out_node}\nRelation target: {self.in_node}\nRelation type: {self.r_type}\nRelation weight: {self.weight}\n"
-
-def parse_relation(line: str) -> R_Relation:
-    line_split = line.split(";")
-    line_split.pop(0)
-    rid = int(line_split.pop(0))
-    out_node = int(line_split.pop(0))
-    in_node = int(line_split.pop(0))
-    r_type = int(line_split.pop(0))
-    weight = int(line_split.pop(0))
-    return R_Relation(rid, out_node, in_node, r_type, weight)
-
-class Node_Type:
-    def __init__(self, type_id: int, type_name: str):
-        self.type_id = type_id 
-        self.type_name = type_name 
-
-    def __str__(self):
-        return f"Node type id : {self.type_id}\nNode type name: {self.type_name}\n"
-
-def parse_type(line: str) -> Node_Type:
-    line_split = line.split(";")
-    line_split.pop(0)
-    type_id = int(line_split.pop(0))
-    type_name = str(line_split.pop(0))
-    return Node_Type(type_id, type_name)
-
-class Relation_Type:
-    def __init__(self, r_type_id: int, r_type_name: str, trgpname: str, r_type_help: str):
-        self.r_type_id = r_type_id 
-        self.r_type_name = r_type_name
-        self.trgpname = trgpname
-        self.r_type_help = r_type_help
-    def __str__(self):
-        return f"Relation type id : {self.r_type_id}\nRelation type name: {self.r_type_name}\nRelation trgpname\nRelation helper string: {self.r_type_help}\n"
+class GraphNode:
+    def __init__(self, value):
+        self.value = value
+        self.index = None
 
 
-def parse_relation_type(line: str) -> Relation_Type:
-    line_split = line.split(";")
-    line_split.pop(0)
-    r_type_id = int(line_split.pop(0))
-    r_type_name = str(line_split.pop(0))
-    trgpname = str(line_split.pop(0))
-    r_type_help = str(line_split.pop(0))
-    return Relation_Type(r_type_id, r_type_name, trgpname, r_type_help)
+def main():
+    """
 
-relations = fetch_word_data("Noah")
-if relations:
+    nodes: list[Node] = []
+    node_types: list[Node_Type] = []
+    relations: list[tuple[int, int, R_Relation]] = []
+    relation_types: list[Relation_Type] = []
 
-    relations_split = relations.split('\n')
+    jdm_data = fetch_word_data("Noah")
 
-    for elem in relations_split:
-        if elem.startswith('nt;'):
-            parse_type(elem)
-        if elem.startswith('rt;'):
-            print(parse_relation_type(elem))
-        if elem.startswith('r;'):
-            parse_relation(elem)
+    if jdm_data:
+
+        relations_split = jdm_data.split('\n')
+
+        for elem in relations_split:
+            if elem.startswith('nt;'):
+                node_type = parse_type(elem)
+                node_types.append(node_type)
+                print(json.dumps(node_type,
+                                 default=lambda x: x.__dict__))
+            if elem.startswith('rt;'):
+                relation_type = parse_relation_type(elem)
+                relation_types.append(relation_type)
+                print(json.dumps(relation_type,
+                                 default=lambda x: x.__dict__))
+            if elem.startswith('r;'):
+                # relation = parse_relation(elem)
+                # relations.append(
+                # (relation.in_node, relation.out_node, relation))
+                # print(json.dumps(relation,
+                # default=lambda x: x.__dict__))
+                pass
+            if elem.startswith('e;'):
+                node = GraphNode(parse_node(elem))
+                print(json.dumps(node,
+                                 default=lambda x: x.__dict__))
+                nodes.append(node)
+
+    graph = rx.PyGraph()
+    for node in nodes:
+        index = graph.add_node(node)
+        graph[index].index = index
+
+    """
+    node_type = Node_Type(1, 'caca')
+
+    node1 = Node(1, 'osef', node_type, 200, '')
+    node2 = Node(2, 'Staline', node_type, 200, '')
+
+    rel_type = Relation_Type(1, 'r_mescouilles', 'osef_mais_en_relation', '')
+
+    relation = R_Relation(1, 1, 2, rel_type, 250)
+
+    graph = rx.PyDiGraph()
+
+    graph.add_node(node1)
+    graph.add_node(node2)
+
+    graph.add_edge(0, 1, relation)
+
+    print(graph.to_dot())
+
+    mpl_draw(graph)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
